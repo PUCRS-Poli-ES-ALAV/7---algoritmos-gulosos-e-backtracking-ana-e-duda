8c84edf6157f3f9f659397f65c21dcd1c08c46b7
Eduarda Rubin <141246640+DudaR18@users.noreply.github.com>
2026-04-22 23:28:50 +0000

    troco

--- Troco.java
+++ Troco.java
@@ -1,5 +1,39 @@
-public Troco {
-    public static Troco (int n) {
-        
+import java.util.ArrayList;
+import java.util.List;
+
+public class Troco {
+
+    public static List<Integer> DaTroco(int[] moedas, int valor) {
+
+        List<Integer> solucao = new ArrayList<>(); // conjunto solução
+        int soma = 0; // soma itens em S
+        int iteracoes = 0;
+
+        for (int i = 0; i < moedas.length; i++) {
+            while (soma + moedas[i] <= valor) {
+                solucao.add(moedas[i]);
+                soma = soma + moedas[i];
+                iteracoes++;
+            }
+        }
+
+        // verifica se atigiu o valor
+        if (soma != valor) {
+            System.out.println("Não tem");
+            return new ArrayList<>();
+        }
+
+        // exibe num iterações
+        System.out.println("Número de iterações: " + iteracoes);
+
+        return solucao;
+    }
+
+    public static void main(String[] args) {
+        int moedas[] = { 100, 20, 10, 5, 1 }; // conjunto de moedas
+        int valor = 289;
+        List<Integer> resultado = DaTroco(moedas, valor);
+
+        System.out.println("Moedas utilizadas: " + resultado);
     }
 }
